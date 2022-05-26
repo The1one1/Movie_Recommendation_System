72ad425b6d6cb089495ffd43c08eb95d1847a852
Rahul Sharma <lci2020029@iiitl.ac.in>
2022-05-26 20:08:24 +0530

    Recommendation bug remove

--- app.py
+++ app.py
@@ -195,7 +195,7 @@ elif output == "Popular":
     genre_id = {"Action": 28, "Adventure": 12, "Animation": 16, "Comedy": 35, "Crime": 80, "Documentary": 99, "Drama": 18, "Family": 10751, "Fantasy": 14, "History": 36,
                 "Horror": 27, "Music": 10402, "Mystery": 9648, "Romance": 10749, "Science Fiction": 878, "TV Movie": 10770, "Thriller": 53, "War": 10752, "Western": 37}
 
-    if st.button("Recommend"):
+    if st.button("Recommend") or st.sidebar.button("Recommend", key="pop"):
         tmdb_movie = Movie()
         result = tmdb_movie.popular()
 
