c39225cff94e1a2ab85d5c762a096400771da074
Rahul Sharma <lci2020029@iiitl.ac.in>
2022-05-27 19:32:10 +0530

    update

--- app.py
+++ app.py
@@ -1,203 +1,46 @@
-from ast import Pass
-import operator
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.metrics.pairwise import linear_kernel
-
-from tmdbv3api import Movie, TMDb
-
+from tmdbv3api import Movie
 import streamlit as st
-from time import time
 
+from time import time
 import pandas as pd
 import numpy as np
-
 import datetime
-import requests
 
-# from sklearn.metrics.pairwise import cosine_similarity
-
-
-tmdb = TMDb()
-tmdb.api_key = 'ef64cf7279087f589625d4f3e0ef5378'
-
-data = pd.read_csv("movie_data.csv")
+import second
 
 
-def title(nevigation):
-
-    if nevigation == 'Movie':
-        st.title('Movie Recommender')
-        return st.selectbox("Enter the movie:(Release till 2016)", data["movie_title"].values)
-    elif nevigation == 'Popular':
-        st.title('Popular Movies')
-    elif nevigation == 'Top Rated':
-        st.title('Top Rated Movies')
-    elif nevigation == 'Rate the Movie':
-        st.title('Give Rating')
-        return st.selectbox("Enter the movie:(Release till 2016)", data["movie_title"].values)
-    else:
-        st.title('Recently Released Movies')
-
-
-def create_sim():
-    data = pd.read_csv('movie_data.csv')
-    # creating a count matrix
-    cv = CountVectorizer(stop_words='english')
-
-    count_matrix = cv.fit_transform(data['comb'])
-    # creating a similarity score matrix
-    # sim = cosine_similarity(count_matrix, count_matrix)
-    sim = linear_kernel(count_matrix, count_matrix)
-    return data, sim
-
-
-def recommend(m):
-    m = m.lower()
-    try:
-        data.head()
-        sim.shape
-    except:
-        data, sim = create_sim()
-    if m not in data['movie_title'].unique():
-        return('Sorry! The movie your searched is not in our database. Please check the spelling or try with some other movies')
-    else:
-        i = data.loc[data['movie_title'] == m].index[0]
-        lst = list(enumerate(sim[i]))
-        lst = sorted(lst, key=lambda x: x[1], reverse=True)
-
-        lst = lst[1:20]
-        l = []
-        for i in range(len(lst)):
-            a = lst[i][0]
-            l.append(data['movie_title'][a])
-        unique_lst = []
-        for x in l:
-            if x not in unique_lst:
-                unique_lst.append(x)
-        unique_lst = unique_lst[:11]
-        return unique_lst
-
-
-def ListOfGenres(genre_json):
-    if genre_json:
-        genres = []
-        genre_str = ", "
-        for i in range(0, len(genre_json)):
-            genres.append(genre_json[i]['name'])
-        return genre_str.join(genres)
-
-
-def date_convert(s):
-    MONTHS = ['January', 'February', 'Match', 'April', 'May', 'June',
-              'July', 'August', 'September', 'October', 'November', 'December']
-    y = s[:4]
-    m = int(s[5:-3])
-    d = s[8:]
-    month_name = MONTHS[m-1]
-
-    result = month_name + ' ' + d + ' ' + y
-    return result
-
-
-def MinsToHours(duration):
-    if duration % 60 == 0:
-        return "{:.0f} hours".format(duration/60)
-    else:
-        return "{:.0f} hours {} minutes".format(duration/60, duration % 60)
-
-
-def get_posters(r):
-    poster = []
-    tmdb_movie = Movie()
-    for movie_title in r:
-        list_result = tmdb_movie.search(movie_title)
-        movie_id = list_result[0].id
-        response = requests.get(
-            'https://api.themoviedb.org/3/movie/{}?api_key={}'.format(movie_id, tmdb.api_key))
-        data_json = response.json()
-        poster.append(
-            'https://image.tmdb.org/t/p/original{}'.format(data_json['poster_path']))
-    return (poster)
-
-
-def movie_details(recommendations):
-
-    for selected_movie_name in recommendations:
-        tmdb_movie = Movie()
-        result = tmdb_movie.search(selected_movie_name)
-
-        # get movie id and movie title
-        movie_id = result[0].id
-        movie_name = result[0].title
-
-        # making API call
-        response = requests.get(
-            'https://api.themoviedb.org/3/movie/{}?api_key={}'.format(movie_id, tmdb.api_key))
-        data_json = response.json()
-        imdb_id = data_json['imdb_id']
-        poster = data_json['poster_path']
-        img_path = 'https://image.tmdb.org/t/p/original{}'.format(poster)
-
-        # getting list of genres form json
-        genre = ListOfGenres(data_json['genres'])
-        # getting votes with comma as thousands separators
-        vote_count = "{:,}".format(result[0].vote_count)
-
-        # convert date to readable format (eg. 10-06-2019 to June 10 2019)
-        try:
-            rd = date_convert(result[0].release_date)
-        except:
-            pass
-
-        # convert minutes to hours minutes (eg. 148 minutes to 2 hours 28 mins)
-        runtime = MinsToHours(data_json['runtime'])
-
-        col1, col2 = st.columns(2)
-        with col1:
-            st.image(img_path)
-        with col2:
-            st.subheader("TITLE: " + selected_movie_name.upper())
-            st.write(result[0].overview)
-            st.write(result[0].vote_average, "("+vote_count+"votes)")
-            st.write(genre)
-            st.write("Release Date: " + rd)
-            st.write("Runtime: "+runtime)
-
-
-output = st.sidebar.radio("Nevigation", [
-                          "Movie", "Popular", "Top Rated", "Recently Released", "Rate the Movie"])
+output = st.sidebar.radio("Nevigation", ["Movie", "Popular", "Top Rated", "Recently Released", "Rate the Movie"])
 
 if output == "Movie":
     # to take input(movie name) from user
-    selected_movie_name = title('Movie')
+    selected_movie_name = second.title('Movie')
 
     if st.button("Recommend"):
-        recommendations = recommend(selected_movie_name)
-        posters = get_posters(recommendations)
+        recommendations = second.recommend(selected_movie_name)
 
         list = []
         list.append(selected_movie_name)
+
         # it will show the selected movie details
-        movie_details(list)
+        second.movie_details(list)
 
         # to display the Poster of recommended movies in sidebar
-        st.sidebar.subheader("Recommendations")
-        st.sidebar.image(posters)
+        second.side_bar_poster(recommendations)
 
         # to show the details of recommended movies
         st.header("Recommendations")
-        movie_details(recommendations)
+        second.movie_details(recommendations)
 
 
 elif output == "Popular":
-    title('Popular')
+    second.title('Popular')
 
     # get popular movies on the basis of genre
-    genre = st.sidebar.selectbox("Select Genre", ["Action", "Adventure", "Animation", "Comedy", "Crime", "Documentary", "Drama", "Family",
-                                                  "Fantasy", "History", "Horror", "Music", "Mystery", "Romance", "Science Fiction", "TV Movie", "Thriller", "War", "Western"])
+    genre = second.get_genre()
 
     genre_id = {"Action": 28, "Adventure": 12, "Animation": 16, "Comedy": 35, "Crime": 80, "Documentary": 99, "Drama": 18, "Family": 10751, "Fantasy": 14, "History": 36,
                 "Horror": 27, "Music": 10402, "Mystery": 9648, "Romance": 10749, "Science Fiction": 878, "TV Movie": 10770, "Thriller": 53, "War": 10752, "Western": 37}
+
 
     if st.button("Recommend") or st.sidebar.button("Recommend", key="pop"):
         tmdb_movie = Movie()
@@ -210,23 +53,21 @@ elif output == "Popular":
             if genre_id[genre] in result[i]['genre_ids']:
                 recommendations.append(result[i].title)
 
+        # if no movie is found
         if len(recommendations) == 0:
-            # if no movie is found
             st.error("No recommendations found")
             st.sidebar.error("No recommendations found")
         else:
             # if movie is found then get the posters then display in sidebar
-            posters = get_posters(recommendations)
-            st.sidebar.subheader("Recommendations")
-            st.sidebar.image(posters)
+            second.side_bar_poster(recommendations)
 
             # to show the details of recommended movies
             st.header("Recommendations")
-            movie_details(recommendations)
+            second.movie_details(recommendations)
 
 
 elif output == "Top Rated":
-    title('Top Rated')
+    second.title('Top Rated')
 
     movie = Movie()
     top_rated_movies = movie.top_rated()
@@ -235,11 +76,12 @@ elif output == "Top Rated":
     for i in range(len(top_rated_movies)):
         movie_list.append(top_rated_movies[i].title)
 
-    movie_details(movie_list)
+    second.side_bar_poster(movie_list)
+    second.movie_details(movie_list)
 
 
 elif output == "Recently Released":
-    title('Recently Released')
+    second.title('Recently Released')
 
     movie = Movie()
     upcoming_movies = movie.upcoming()
@@ -247,12 +89,13 @@ elif output == "Recently Released":
     for i in range(len(upcoming_movies)):
         movie_list.append(upcoming_movies[i].title)
 
-    movie_details(movie_list)
+    second.side_bar_poster(movie_list)
+    second.movie_details(movie_list)
 
 
 elif output == "Rate the Movie":
 
-    movie_name = title('Rate the Movie')
+    movie_name = second.title('Rate the Movie')
     st.subheader("Rating of the movie: " + movie_name)
 
     # slider to get the rating of the movie
@@ -268,7 +111,6 @@ elif output == "Rate the Movie":
     user = st.text_input("Enter your name:")
 
     if st.button("Rate"):
-
         data = pd.read_csv("users_movie_rating.csv")
 
         # add the data to particular row and particular column of csv file
@@ -284,11 +126,7 @@ elif output == "Rate the Movie":
                 if np.isnan(data['vote_count'].values[i]):
                     data['vote_count'].values[i] = 0.0
 
-                # average of rating
-                x = ((data["rating"].values[i] * data['vote_count'].values[i]
-                      ) + float(rating)) / (data['vote_count'].values[i] + 1.0)
-
-                data["rating"].values[i] = x
+                data["rating"].values[i] = second.avg_rating(data, rating, i)
                 data["date"].values[i] = date
                 data["time"].values[i] = time
                 data["user"].values[i] = user
@@ -298,26 +136,14 @@ elif output == "Rate the Movie":
                 data.to_csv("users_movie_rating.csv", index=False)
                 break
 
-        st.success("Data Submitted")
-        st.write("Thanks for rating the movie")
-        st.write("Your contribution will help us to improve our app")
-
-        # to display the rating of the movie in sidebar
-        st.sidebar.subheader("Movie: " + movie_name)
-        # show rating of the movie movie_name
-        st.sidebar.write(
-            "Rating = ", data[data["movie_title"] == movie_name]["rating"].values[0])
-
-        # no of votes of the movie movie_name
-        st.sidebar.write("No of Votes = ", int(
-            data[data["movie_title"] == movie_name]["vote_count"].values[0]))
+        second.success_message(data, movie_name)
 
 
 
 # I want to display the top rated movies from the my database csv files but now 
-# sufficient votes has given to the movies.
-# Once each movie get enough votes then it will be displayed in the top rated movies section from app database.
-# till now the movies are not displayed in the top rated movies section from the databse of tmdb .
+# sufficient votes has been not given to the movies.
+# Once each movie get enough votes then I will displayed the "top rated movies section" from app database.
+# till now the movies are displayed in the "top rated movies section" from the databse of tmdb .
 
 
 # tp rated movies from users_movie_rating.csv
---
+++ second.py
@@ -0,0 +1,164 @@
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import linear_kernel
+from tmdbv3api import Movie, TMDb
+import streamlit as st
+import pandas as pd
+
+import Converter
+import requests
+
+tmdb = TMDb()
+tmdb.api_key = 'ef64cf7279087f589625d4f3e0ef5378'
+
+
+def title(nevigation):
+    data = pd.read_csv("movie_data.csv")
+    if nevigation == 'Movie':
+        st.title('Movie Recommender')
+        return st.selectbox("Enter the movie:(Release till 2016)", data["movie_title"].values)
+    elif nevigation == 'Popular':
+        st.title('Popular Movies')
+    elif nevigation == 'Top Rated':
+        st.title('Top Rated Movies')
+    elif nevigation == 'Rate the Movie':
+        st.title('Give Rating')
+        return st.selectbox("Enter the movie:(Release till 2016)", data["movie_title"].values)
+    else:
+        st.title('Recently Released Movies')
+
+
+def create_sim():
+    data = pd.read_csv('movie_data.csv')
+    # creating a count matrix
+    cv = CountVectorizer(stop_words='english')
+
+    count_matrix = cv.fit_transform(data['comb'])
+    # creating a similarity score matrix
+    sim = linear_kernel(count_matrix, count_matrix)
+    return data, sim
+
+
+def get_posters(r):
+    poster = []
+    tmdb_movie = Movie()
+    for movie_title in r:
+        list_result = tmdb_movie.search(movie_title)
+        movie_id = list_result[0].id
+        response = requests.get(
+            'https://api.themoviedb.org/3/movie/{}?api_key={}'.format(movie_id, tmdb.api_key))
+        data_json = response.json()
+        poster.append(
+            'https://image.tmdb.org/t/p/original{}'.format(data_json['poster_path']))
+    return (poster)
+
+
+def movie_details(recommendations):
+
+    for selected_movie_name in recommendations:
+        tmdb_movie = Movie()
+        result = tmdb_movie.search(selected_movie_name)
+
+        # get movie id and movie title
+        movie_id = result[0].id
+        # movie_name = result[0].title
+
+        # making API call
+        response = requests.get(
+            'https://api.themoviedb.org/3/movie/{}?api_key={}'.format(movie_id, tmdb.api_key))
+        data_json = response.json()
+        poster = data_json['poster_path']
+        img_path = 'https://image.tmdb.org/t/p/original{}'.format(poster)
+
+        # getting list of genres form json
+        genre = Converter.ListOfGenres(data_json['genres'])
+
+        # getting votes with comma as thousands separators
+        vote_count = "{:,}".format(result[0].vote_count)
+
+        # convert date to readable format (eg. 10-06-2019 to June 10 2019)
+        try:
+            rd = Converter.date_convert(result[0].release_date)
+        except:
+            pass
+
+        # convert minutes to hours minutes (eg. 148 minutes to 2 hours 28 mins)
+        runtime = Converter.MinsToHours(data_json['runtime'])
+
+        col1, col2 = st.columns(2)
+        with col1:
+            st.image(img_path)
+        with col2:
+            st.subheader("TITLE: " + selected_movie_name.upper())
+            st.write(result[0].overview)
+            st.write(result[0].vote_average, "("+vote_count+"votes)")
+            st.write(genre)
+            st.write("Release Date: " + rd)
+            st.write("Runtime: "+runtime)
+
+
+def side_bar_poster(movie_list):
+    posters = get_posters(movie_list)
+    st.sidebar.subheader("Recommendations")
+    st.sidebar.image(posters)
+
+
+def recommend(m):
+    data = pd.read_csv("movie_data.csv")
+    m = m.lower()
+    try:
+        data.head()
+        sim.shape
+    except:
+        data, sim = create_sim()
+    if m not in data['movie_title'].unique():
+        return('Sorry! The movie your searched is not in our database. Please check the spelling or try with some other movies')
+    else:
+        i = data.loc[data['movie_title'] == m].index[0]
+        lst = list(enumerate(sim[i]))
+        lst = sorted(lst, key=lambda x: x[1], reverse=True)
+
+        lst = lst[1:20]
+        l = []
+        for i in range(len(lst)):
+            a = lst[i][0]
+            l.append(data['movie_title'][a])
+        unique_lst = []
+        for x in l:
+            if x not in unique_lst:
+                unique_lst.append(x)
+        unique_lst = unique_lst[:11]
+        return unique_lst
+
+
+def get_genre():
+    genre = st.sidebar.selectbox("Select Genre", ["Action", "Adventure", "Animation", "Comedy", "Crime", "Documentary", "Drama", "Family",
+        "Fantasy", "History", "Horror", "Music", "Mystery", "Romance", "Science Fiction", "TV Movie", "Thriller", "War", "Western"])
+    return genre
+
+
+def success_message(data, movie_name):
+    st.success("Data Submitted")
+    st.write("Thanks for rating the movie")
+    st.write("Your contribution will help us to improve our app")
+
+    # to display the rating of the movie in sidebar
+    st.sidebar.subheader("Movie: " + movie_name)
+
+    # show rating of the movie movie_name
+    st.sidebar.write(
+        "Rating = ", data[data["movie_title"] == movie_name]["rating"].values[0])
+
+    # no of votes of the movie movie_name
+    st.sidebar.write("No of Votes = ", int(
+        data[data["movie_title"] == movie_name]["vote_count"].values[0]))
+
+
+def avg_rating(data, rating, i):
+    # average of rating
+    total_rating = (data["rating"].values[i] * data['vote_count'].values[i])
+
+    # after adding new rating
+    total_rating += rating
+
+    # average of rating
+    return total_rating / (data['vote_count'].values[i] + 1.0)
